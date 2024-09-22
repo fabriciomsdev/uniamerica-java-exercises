28ab23395451dbe07e7b45a84d175462ad4372fc
Fabricio Magalhães <fabricioms.dev@gmail.com>
2024-09-22 14:48:18 -0300

    feat: simple sum

---
+++ sum.java
@@ -0,0 +1,11 @@
+class Sum {
+    public static void main(String[] args) {
+        // Define two integers
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        int result = a + b;
+
+        System.out.println(result);
+    }
+}
